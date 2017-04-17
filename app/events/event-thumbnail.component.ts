4e6bd4bf6df9292ee4a0bc070357401c85f0c422
Bruno Quintella <bruno.quintella@bravi.com.br>
2017-04-17 18:21:54 +0100

    Create a child component to be responsible for show the event data

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -1,15 +1,21 @@
-<div>
-    <h1>Upcoming Angular 2 Events</h1>
-    <hr />
+import { Component, Input } from '@angular/core'
+
+@Component({
+    selector: 'event-thumbnail',
+    template: `
     <div class="well hoverwell thumbnail">
         <h2>{{event.name}}</h2>
         <div>Date: {{event.date}}</div>
         <div>Time: {{event.time}}</div>
-        <div>Price: ${{event.price}}</div>
+        <div>Price: \${{event.price}}</div>
         <div>
             <span>Locatio: {{event.location.address}}</span>
             <span>&nbsp;</span>
             <span>{{event.location.city}}, {{event.location.country}}</span>
         </div>
     </div>
-</div>
+    `
+})
+export class EventThumbnailComponent {
+    @Input() event:any;
+}
