812f31071f055ae563c7c15955d41098ad7f6d42
Bruno Quintella <bruno.quintella@bravi.com.br>
2017-06-15 19:38:36 +0100

    Create the application routes

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,21 +1,29 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { RouterModule } from '@angular/router'
+
+import { appRoutes } from './routes'
 
 import { EventsAppComponent } from './events-app.component'
 import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/navbar.component'
+import { EventDetailsComponent } from './events/event-details/event-details.component'
 
 import { EventService } from './events/shared/event.service'
 import { ToastrService } from './common/toastr.service'
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes)
+        ],
     declarations: [
         EventsAppComponent,
         EventsListComponent,
         EventThumbnailComponent,
-        NavBarComponent
+        NavBarComponent,
+        EventDetailsComponent
     ],
     providers: [EventService, ToastrService],
     bootstrap: [EventsAppComponent]
